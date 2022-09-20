275a052915c8e7d936da457654630b601e952140
JDGamingPS <gamingprm1@gmail.com>
2022-09-19 21:15:38 -0400

    vue js cli 3 templates

---
+++ 06-Componentes/app-practica/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- 
+      header
+      main
+        img - imagenes[props] {global}
+      footer
+     -->
+    <div class="container">
+      <div class="row">
+          <h1>hola</h1>
+        
+      </div>
+  
+      <div class="row">
+        <h2> 
+          hola dos
+        </h2>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+@import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+</style>
